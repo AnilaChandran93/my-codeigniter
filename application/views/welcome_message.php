74d5b70c0096e96b96533bdd7f661b0e0f33db63
SHEKHAR <SHEKHAR@ShekharJoshi>
2015-03-22 20:43:14 +0530

    initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('/resources/css/bootstrap.min.css');?>"/>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+        
+        
+        <iframe class="vid" width="560" height="315" src="https://www.youtube.com/embed/0bZlqoZ6B4U" frameborder="0" allowfullscreen></iframe>
+        <img src="<?php echo base_url('/resources/images/imgres.jpg')?>"/>        
+</div>
+
+</body>
+<script type="text/javascript" src="<?php echo base_url('/resources/js/jquery-2.1.3.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/resources/js/bootstrap.min.js')?>"></script>
+</html>
